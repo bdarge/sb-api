96b09c24fe383fc97f9515623e2b1b61c16d239a
bdarge <bin.darge@gmail.com>
2025-01-19 17:57:16 -0500

    update logs

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -7,12 +7,14 @@ import (
 	"log"
 )
 
+// Handler db handler
 type Handler struct {
 	DB *gorm.DB
 }
 
+// Init database connection
 func Init(url string) Handler {
-	log.Printf("Initialize db")
+	log.Printf("Initialize db connection")
 	db, err := gorm.Open(mysql.Open(url), &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Info),
 	})
